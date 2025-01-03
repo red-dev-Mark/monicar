429f4dab2ab82f92224a78f59a6ffd8fba6cf345
nanafromjeju <nanafromjeju@gmail.com>
2025-01-04 01:15:43 +0900

    feat: SearchInput 공통 컴포넌트 구현 (#26) ------------------------------------------------------
    ------------------------------------------------------

---
+++ components/common/Input/SearchInput/SearchInput.stories.tsx
@@ -0,0 +1,69 @@
+import SearchInput from './index'
+
+import type { Meta, StoryObj } from '@storybook/react'
+
+const meta: Meta<typeof SearchInput> = {
+    title: 'Components/Input/SearchInput',
+    component: SearchInput,
+    parameters: {
+        layout: 'centered',
+    },
+    tags: ['autodocs'],
+    argTypes: {
+        size: {
+            control: 'select',
+            options: ['small', 'medium', 'large'],
+            description: '검색 입력창의 크기를 설정합니다',
+        },
+        placeholder: {
+            control: 'text',
+            description: '플레이스홀더 텍스트를 설정합니다',
+        },
+        disabled: {
+            control: 'boolean',
+            description: '비활성화 상태를 설정합니다',
+        },
+    },
+    decorators: [
+        (Story) => (
+            <div style={{ width: '320px' }}>
+                <Story />
+            </div>
+        ),
+    ],
+}
+
+export default meta
+
+type StoryType = StoryObj<typeof SearchInput>
+export const Default: StoryType = {
+    args: {
+        placeholder: '검색어를 입력하세요',
+        size: 'medium',
+        icon: '/icons/search-icon.svg',
+    },
+}
+
+export const PinkSearch: StoryType = {
+    args: {
+        placeholder: '차량을 검색하세요.',
+        size: 'small',
+        icon: '/icons/pink-search-icon.svg',
+    },
+}
+
+export const WhiteSearch: StoryType = {
+    args: {
+        placeholder: '차량을 검색하세요.',
+        size: 'medium',
+        icon: '/icons/search-icon.svg',
+    },
+}
+
+export const BlackSearch: StoryType = {
+    args: {
+        placeholder: '차량을 검색하세요.',
+        size: 'large',
+        icon: '/icons/black-search-icon.svg',
+    },
+}
