14c72431192fd359c71c5f2425fa445293a68221
nanafromjeju <nanafromjeju@gmail.com>
2025-01-04 21:14:42 +0900

    style: Navigation 컴포넌트 모바일 반응형 작업 (#18) ------------------------------------------------------
    ------------------------------------------------------

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,9 @@
-import BaseInput from '@/components/common/Input/BaseInput'
-import SignInInput from '@/components/common/Input/SignInInput'
 import Header from '@/components/layout/Header'
 
 const HomePage = () => {
     return (
         <>
-            <Header title='asdfasd' isBackButton />
-            <SignInInput icon={'/icons/sign-in-user-icon.svg'} />
-            <SignInInput icon={'/icons/sign-in-lock-icon.svg'} type='password' />
-            <BaseInput />
+            <Header title='' isBackButton />
         </>
     )
 }
