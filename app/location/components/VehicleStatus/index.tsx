0cb3a0b5fcb33760779247262f021161dd60f11d
Mark Kwon <redhero8830@gmail.com>
2025-01-06 20:16:30 +0900

    feat: 위치 조회 페이지 컴포넌트 폴더 구조 작성 (#34)
    ------------------------------------------------------
    
    ------------------------------------------------------

---
+++ app/location/components/VehicleStatus/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const VehicleStatus = () => {
+    return <div>Map</div>
+}
+
+export default VehicleStatus
--- app/location/page.tsx
+++ app/location/page.tsx
@@ -1,16 +1,15 @@
-'use client'
-
-import { Map } from 'react-kakao-maps-sdk'
-
-import { useKakaoLoader } from '@/hooks/useKakaoLoader'
+import Map from '@/app/location/components/Map'
+import SearchInput from '@/components/common/Input/SearchInput'
 
 const LocationPage = () => {
-    const { loading, error } = useKakaoLoader()
-
-    if (loading) return <div>지도를 불러오는 중...</div>
-    if (error) return <div>지도를 불러오는데 실패했습니다</div>
-
-    return <Map center={{ lat: 37.5665, lng: 126.978 }} style={{ width: '100%', height: '100vh' }} level={3} />
+    return (
+        <div>
+            <Map />
+            <div>
+                <SearchInput />
+            </div>
+        </div>
+    )
 }
 
 export default LocationPage
