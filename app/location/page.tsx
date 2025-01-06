3fd3ef7716be0e108d70c298cc16f10143eb64c6
Mark Kwon <redhero8830@gmail.com>
2025-01-06 16:53:15 +0900

    feat<map>: 카카오맵 API 연동 및 로드 커스텀훅 useKakaoLoader 구현 (#34)
    ------------------------------------------------------
    
    ------------------------------------------------------

--- app/location/page.tsx
+++ app/location/page.tsx
@@ -1,7 +1,16 @@
 'use client'
 
+import { Map } from 'react-kakao-maps-sdk'
+
+import { useKakaoLoader } from '@/hooks/useKakaoLoader'
+
 const LocationPage = () => {
-    return <div>pagghgfe</div>
+    const { loading, error } = useKakaoLoader()
+
+    if (loading) return <div>지도를 불러오는 중...</div>
+    if (error) return <div>지도를 불러오는데 실패했습니다</div>
+
+    return <Map center={{ lat: 37.5665, lng: 126.978 }} style={{ width: '100%', height: '100vh' }} level={3} />
 }
 
 export default LocationPage
