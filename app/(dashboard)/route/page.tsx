0f16d4b18fb49ce1a0faf38ae74622ac1b50dec3
Mark Kwon <redhero8830@gmail.com>
2025-01-07 15:11:45 +0900

    design(map): 차량 검색 / 기간 검색 컴포넌트 구현 (#41) ------------------------------------------------------
    ------------------------------------------------------

--- app/(dashboard)/route/page.tsx
+++ app/(dashboard)/route/page.tsx
@@ -1,4 +1,5 @@
 import Map from '@/app/(dashboard)/location/components/Map'
+import { BaseButton } from '@/components/common/Button/BaseButton'
 import SearchInput from '@/components/common/Input/SearchInput'
 
 import * as styles from './styles.css'
@@ -7,12 +8,69 @@ const RoutePage = () => {
     return (
         <div className={styles.container}>
             <Map />
-            <div className={styles.searchInputWrapper}>
-                <SearchInput icon='/icons/search-icon.svg' size='large' />
+            <div className={styles.searchContainer}>
+                <div className={styles.vehicleNumberSearchContainer}>
+                    <h3 className={styles.title}>차량 검색</h3>
+                    <SearchInput
+                        style={{
+                            borderRadius: '10px',
+                        }}
+                        className={styles.input}
+                        icon='/icons/pink-search-icon.svg'
+                        placeholder='차량번호 검색'
+                        size='large'
+                    />
+                </div>
+
+                <div className={styles.periodSearch}>
+                    <h3 className={styles.title}>기간 검색</h3>
+                    <p className={styles.label}>시작 일시</p>
+                    <div className={styles.selectGroup}>
+                        <select className={styles.select}>
+                            <option>2024년</option>
+                        </select>
+                        <select className={styles.select}>
+                            <option>05월</option>
+                        </select>
+                        <select className={styles.select}>
+                            <option>06월</option>
+                        </select>
+                        <select className={styles.select}>
+                            <option>00시</option>
+                        </select>
+                        <select className={styles.select}>
+                            <option>30분</option>
+                        </select>
+                    </div>
+                    <p className={styles.label}>종료 일시</p>
+                    <div className={styles.selectGroup}>
+                        <select className={styles.select}>
+                            <option>2024년</option>
+                        </select>
+                        <select className={styles.select}>
+                            <option>05월</option>
+                        </select>
+                        <select className={styles.select}>
+                            <option>06월</option>
+                        </select>
+                        <select className={styles.select}>
+                            <option>00시</option>
+                        </select>
+                        <select className={styles.select}>
+                            <option>30분</option>
+                        </select>
+                    </div>
+                </div>
+
+                <BaseButton
+                    style={{
+                        width: '100%',
+                        height: '54px',
+                    }}
+                >
+                    경로 조회하기
+                </BaseButton>
             </div>
-            {/* <div className={styles.vehicleStatusWrapper}> */}
-            {/* <VehicleStatus /> */}
-            {/* </div> */}
         </div>
     )
 }
--- app/(dashboard)/route/styles.css.ts
+++ app/(dashboard)/route/styles.css.ts
@@ -1,21 +1,79 @@
 import { style } from '@vanilla-extract/css'
 
+import { styles } from '@/styles/theme.css'
+
 export const container = style({
     position: 'relative',
     width: '100%',
     height: '100vh',
+    color: styles.colors.gray[800],
 })
 
-export const searchInputWrapper = style({
-    position: 'absolute',
-    top: '2rem',
-    right: '2rem',
-    zIndex: 10,
+export const input = style({
+    width: '100% !important',
 })
 
-export const vehicleStatusWrapper = style({
+export const searchContainer = style({
+    padding: '24px',
     position: 'absolute',
-    top: '6.5rem',
-    right: '2rem',
+    top: '1.5rem',
+    right: '1.5rem',
     zIndex: 10,
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '24px',
+
+    backgroundColor: 'rgba(255, 255, 255, 0.95)',
+    borderRadius: '16px',
+    boxShadow: '0 4px 12px rgba(0, 0, 0, 0.1)',
+})
+
+export const vehicleNumberSearchContainer = style({
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '8px',
+})
+export const periodSearch = style({
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '8px',
+})
+
+export const title = style({
+    fontSize: '18px',
+    // fontSize: styles.fontSizes.large,
+    fontWeight: styles.fontWeights.bold,
+    color: styles.colors.gray[800],
+    marginBottom: '8px',
+})
+
+export const label = style({
+    fontSize: '14px',
+    color: styles.colors.gray[600],
+    marginBottom: '4px',
+})
+
+export const selectGroup = style({
+    display: 'flex',
+    gap: '8px',
+    marginBottom: '4px',
+})
+
+export const select = style({
+    // select 요소용 스타일 추가
+    padding: '8px',
+    borderRadius: '8px',
+    border: `1px solid ${styles.colors.gray[200]}`,
+    backgroundColor: 'white',
+    fontSize: '14px',
+    color: styles.colors.gray[700],
+    cursor: 'pointer',
+    ':hover': {
+        borderColor: styles.colors.gray[300],
+    },
+    ':focus': {
+        outline: 'none',
+        borderColor: styles.colors.primary,
+        boxShadow: `0 0 0 2px ${styles.colors.primary}25`,
+    },
 })
