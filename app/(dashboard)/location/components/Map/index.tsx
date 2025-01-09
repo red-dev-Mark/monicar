21867974953576fc08178534b79fab6d2bfea8a3
Mark Kwon <redhero8830@gmail.com>
2025-01-09 10:19:43 +0900

    feat(vehicle): 개별 차량 정보 표시 CustomOverlayMap 구현 (#46) ------------------------------------------------------
    ------------------------------------------------------

--- app/(dashboard)/location/components/Map/index.tsx
+++ app/(dashboard)/location/components/Map/index.tsx
@@ -11,14 +11,14 @@ interface MapProps {
     children?: React.ReactNode
 }
 
-const Map = ({ center = { lat: 37.5665, lng: 126.978 }, zoom = 10, children }: MapProps) => {
+const Map = ({ center = { lat: 37.5665, lng: 126.978 }, zoom = 10, children, ...props }: MapProps) => {
     const { loading, error } = useKakaoLoader()
 
     if (loading) return <div>지도를 불러오는 중...</div>
     if (error) return <div>지도를 불러오는데 실패했습니다</div>
 
     return (
-        <KakaoMap center={center} level={zoom} style={{ width: '100%', height: '100%' }}>
+        <KakaoMap center={center} level={zoom} style={{ width: '100%', height: '100%' }} {...props}>
             {children}
         </KakaoMap>
     )
