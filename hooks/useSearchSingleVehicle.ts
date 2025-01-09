c4e741520f8ac36250e2fac32fe1bc9e8c23f71b
Mark Kwon <redhero8830@gmail.com>
2025-01-09 15:27:03 +0900

    feat(vehicle): 개별 차량 검색 유효성 검사 `validateVehicleNumber` 유틸함수 구현 (#46) ------------------------------------------------------
    ------------------------------------------------------

--- hooks/useSearchSingleVehicle.ts
+++ hooks/useSearchSingleVehicle.ts
@@ -1,10 +1,11 @@
 import { useEffect, useState } from 'react'
 
 import { INITIAL_MAP_STATE } from '@/constants/map'
+import { validateVehicleNumber } from '@/lib/validation'
 import mockData from '@/mock/single_vehicle_search_ok.json'
 import { singleVehicleModel } from '@/types/vehicle'
 
-export const useSearchSingleVehicle = () => {
+export const useSearchSingleVehicle = (searchTerm: string) => {
     const [singleVehicle, setSingleVehicle] = useState<singleVehicleModel>()
     const [mapState, setMapState] = useState(INITIAL_MAP_STATE)
     const [showSingleVehicle, setShowSingleVehicle] = useState(false)
@@ -29,6 +30,19 @@ export const useSearchSingleVehicle = () => {
 
     const searchSingleVehicle = () => {
         if (!singleVehicle) return
+
+        const validation = validateVehicleNumber(searchTerm)
+
+        if (!validation.isValid) {
+            alert(validation.message)
+            return
+        }
+
+        // TODO: API 호출 로직으로 변경하기
+        if (validation.value !== mockData.vehicle.vehicleNumber) {
+            alert('등록되지 않은 차량번호입니다.')
+            return
+        }
 
         setMapState({
             center: {
