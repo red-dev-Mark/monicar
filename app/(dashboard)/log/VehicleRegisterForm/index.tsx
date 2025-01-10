27c1b1e74bb55553739372fd2fd42a60b0b81c2f
nanafromjeju <nanafromjeju@gmail.com>
2025-01-10 15:09:41 +0900

    refactor: `VehicleRegisterForm` 컴포넌트 구조 개선 (#48)
    - 중복되는 폼 구조를 `VehicleRegisterForm` 컴포넌트로 통합
    - 컴포넌트를 `props`로 받아 재사용성 향상
    ------------------------------------------------------
    
    ------------------------------------------------------

--- app/(dashboard)/log/VehicleRegisterForm/index.tsx
+++ app/(dashboard)/log/VehicleRegisterForm/index.tsx
@@ -1,60 +1,25 @@
 'use client'
 
-import { Select } from '@mantine/core'
-
-import BaseInput from '@/components/common/Input/BaseInput'
-import SearchInput from '@/components/common/Input/SearchInput'
 import Message from '@/components/common/Message'
-import { MESSAGES } from '@/components/common/Message/constants'
 
 import * as styles from './styles.css'
 
 interface VehicleRegisterFormProps {
+    label: string
     isError?: boolean
+    children: React.ReactNode
 }
 
-const VehicleRegisterForm = ({ isError = false }: VehicleRegisterFormProps) => {
+const VehicleRegisterForm = ({ label, isError, children }: VehicleRegisterFormProps) => {
     return (
         <div className={styles.container}>
             <div className={styles.contentsWrapper}>
                 <div className={styles.fieldWrapper}>
-                    <div className={styles.textWrapper}>차량번호</div>
-                    <div className={styles.inputWrapper}>
-                        <SearchInput icon='/icons/pink-search-icon.svg' />
-                    </div>
+                    <div className={styles.textWrapper}>{label}</div>
+                    <div className={styles.inputWrapper}>{children}</div>
                 </div>
 
-                {isError && <Message message={MESSAGES.ERROR.REGISTRATION_UNAVAILABLE} />}
-            </div>
-
-            <div className={styles.contentsWrapper}>
-                <div className={styles.fieldWrapper}>
-                    <div className={styles.textWrapper}>차량종류</div>
-                    <div className={styles.inputWrapper}>
-                        <Select></Select>
-                    </div>
-                </div>
-                {isError && <Message message={MESSAGES.ERROR.VEHICLE_SELECTION_REQUIRED} />}
-            </div>
-
-            <div className={styles.contentsWrapper}>
-                <div className={styles.fieldWrapper}>
-                    <div className={styles.textWrapper}>주행거리</div>
-                    <div className={styles.inputWrapper}>
-                        <BaseInput placeholder='0km' />
-                    </div>
-                </div>
-                {isError && <Message message={MESSAGES.ERROR.MILEAGE_REQUIRED} />}
-            </div>
-
-            <div className={styles.contentsWrapper}>
-                <div className={styles.fieldWrapper}>
-                    <div className={styles.textWrapper}>출고일</div>
-                    <div className={styles.inputWrapper}>
-                        <BaseInput placeholder='0000-00-00' />
-                    </div>
-                </div>
-                {isError && <Message message={MESSAGES.ERROR.DATE_REQUIRED} />}
+                {isError && <Message message={''} />}
             </div>
         </div>
     )
