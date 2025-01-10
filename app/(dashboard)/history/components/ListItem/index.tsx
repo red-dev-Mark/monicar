ebb70b12ee7b3e081fe41ab7ddc30554ed8adc1b
nanafromjeju <nanafromjeju@gmail.com>
2025-01-10 23:02:11 +0900

    style(history): ListItem 컴포넌트 스타일명 변경 (#37) ------------------------------------------------------
    ------------------------------------------------------

--- app/(dashboard)/history/components/ListItem/index.tsx
+++ app/(dashboard)/history/components/ListItem/index.tsx
@@ -20,15 +20,15 @@ const ListItem = ({ data }: ListItemProps) => {
     } = data
 
     return (
-        <div className={styles.itemWrapper}>
-            <span className={styles.itemContent}>{vehicleNumber}</span>
-            <span className={styles.itemContent}>{department}</span>
-            <span className={styles.itemContent}>{name}</span>
-            <span className={styles.itemContent}>{drivingDays}일</span>
-            <span className={styles.itemContent}>{averageDrivingDistance}km</span>
-            <span className={styles.itemContent}>{averageDrivingTime}분</span>
-            <span className={styles.itemContent}>{totalDrivingDistance}km</span>
-            <span className={styles.itemContent}>{drivingRate}%</span>
+        <div className={styles.container}>
+            <div className={styles.itemWrapper}>{vehicleNumber}</div>
+            <span className={styles.itemWrapper}>{department}</span>
+            <span className={styles.itemWrapper}>{name}</span>
+            <span className={styles.itemWrapper}>{drivingDays}일</span>
+            <span className={styles.itemWrapper}>{averageDrivingDistance}km</span>
+            <span className={styles.itemWrapper}>{averageDrivingTime}분</span>
+            <span className={styles.itemWrapper}>{totalDrivingDistance}km</span>
+            <span className={styles.itemWrapper}>{drivingRate}%</span>
             <div className={styles.icon}>{icon}</div>
         </div>
     )
