49d84d4e003c8cdddb8e233e81cf6cc9462ec252
Mark Kwon <redhero8830@gmail.com>
2025-01-13 01:48:44 +0900

    refactor(search): DateTimeSelect 컴포넌트 중복 Select 구조 개선 (#56)
    - dateTimeFields로 각 Select에 필요한 데이터 관리

--- app/(dashboard)/route/components/RouteSearchPanel/DateTimeSelect/index.tsx
+++ app/(dashboard)/route/components/RouteSearchPanel/DateTimeSelect/index.tsx
@@ -8,93 +8,55 @@ import { DateTime } from '@/app/(dashboard)/route/types/date'
 import { YEAR_OPTIONS, MONTH_OPTIONS, DAY_OPTIONS, HOUR_OPTIONS, MINUTE_OPTIONS } from './constants'
 import * as styles from './styles.css'
 
-interface DateTimeSelectProps {
-    label: string
-    disabled: boolean
-    value: DateTime
-    onChange: Dispatch<SetStateAction<DateTime>>
+interface DateTimeField {
+    value: keyof DateTime
+    data: {
+        value: string
+        label: string
+    }[]
+    placeholder: string
 }
 
-const DateTimeSelect = ({ label, disabled, value, onChange }: DateTimeSelectProps) => {
+interface DateTimeSelectProps {
+    label: string
+    value: DateTime
+    onChange: Dispatch<SetStateAction<DateTime>>
+    disabled: boolean
+}
+
+const DateTimeSelect = ({ label, value, onChange, disabled }: DateTimeSelectProps) => {
+    const dateTimeFields: DateTimeField[] = [
+        { value: 'year', data: YEAR_OPTIONS, placeholder: '2025년' },
+        { value: 'month', data: MONTH_OPTIONS, placeholder: '01월' },
+        { value: 'date', data: DAY_OPTIONS, placeholder: '01일' },
+        { value: 'hour', data: HOUR_OPTIONS, placeholder: '00시' },
+        { value: 'minute', data: MINUTE_OPTIONS, placeholder: '00분' },
+    ]
+
     return (
         <div className={styles.selectContainer}>
             <p className={styles.label}>{label}</p>
             <div className={styles.selectGroup}>
-                <Select
-                    value={value.year}
-                    onChange={(selected) =>
-                        onChange((prev) => ({
-                            ...prev,
-                            year: selected || '',
-                        }))
-                    }
-                    placeholder='2025년'
-                    disabled={disabled}
-                    data={YEAR_OPTIONS}
-                    styles={styles.selectStyles}
-                    withCheckIcon={false}
-                    allowDeselect={false}
-                />
-                <Select
-                    value={value.month}
-                    onChange={(selected) =>
-                        onChange((prev) => ({
-                            ...prev,
-                            month: selected || '',
-                        }))
-                    }
-                    data={MONTH_OPTIONS}
-                    placeholder='01월'
-                    disabled={disabled}
-                    styles={styles.selectStyles}
-                    withCheckIcon={false}
-                    allowDeselect={false}
-                />
-                <Select
-                    value={value.date}
-                    onChange={(selected) =>
-                        onChange((prev) => ({
-                            ...prev,
-                            date: selected || '',
-                        }))
-                    }
-                    data={DAY_OPTIONS}
-                    placeholder='01일'
-                    disabled={disabled}
-                    styles={styles.selectStyles}
-                    withCheckIcon={false}
-                    allowDeselect={false}
-                />
-                <Select
-                    value={value.hour}
-                    onChange={(selected) =>
-                        onChange((prev) => ({
-                            ...prev,
-                            hour: selected || '',
-                        }))
-                    }
-                    data={HOUR_OPTIONS}
-                    placeholder='00시'
-                    disabled={disabled}
-                    styles={styles.selectStyles}
-                    withCheckIcon={false}
-                    allowDeselect={false}
-                />
-                <Select
-                    value={value.minute}
-                    onChange={(selected) =>
-                        onChange((prev) => ({
-                            ...prev,
-                            minute: selected || '',
-                        }))
-                    }
-                    data={MINUTE_OPTIONS}
-                    placeholder='00분'
-                    disabled={disabled}
-                    styles={styles.selectStyles}
-                    withCheckIcon={false}
-                    allowDeselect={false}
-                />
+                {dateTimeFields.map((field) => {
+                    return (
+                        <Select
+                            key={field.value}
+                            data={field.data}
+                            placeholder={field.placeholder}
+                            value={value[field.value]}
+                            onChange={(selected) =>
+                                onChange((prev) => ({
+                                    ...prev,
+                                    [field.value]: selected || '',
+                                }))
+                            }
+                            disabled={disabled}
+                            styles={styles.selectStyles}
+                            withCheckIcon={false}
+                            allowDeselect={false}
+                        />
+                    )
+                })}
             </div>
         </div>
     )
