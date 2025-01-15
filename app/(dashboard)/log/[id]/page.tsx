f1566d7fc5fbc0d229186b0b52ee7aee68246ea1
nanafromjeju <nanafromjeju@gmail.com>
2025-01-15 18:25:22 +0900

    remove(log): VehicleInfo 컴포넌트 삭제 (#59)

--- app/(dashboard)/log/[id]/page.tsx
+++ app/(dashboard)/log/[id]/page.tsx
@@ -1,40 +1,31 @@
 'use client'
 
-import VehicleInfo from '../components/VehicleInfo'
-
-const vehicleDetailData = {
-    vehicleNumber: '123가4567',
-    vehicleModel: '제네시스',
-    vehicleData: {
-        taxStartPeriod: '2024-01-01',
-        taxEndPeriod: '2024-12-31',
-        businessId: 1,
-        businessName: '테스트 주식회사',
-        businessRegistrationNumber: '123-45-67890',
-        vehicleNumber: '123가4567',
-        vehicleTypesName: '제네시스 G80',
-        usageDate: '2024-01-14',
-        userId: 1,
-        departmentName: '영업팀',
-        name: '홍길동',
-        mileageBefore: 50000,
-        mileageAfter: 50150,
-        totalMileage: 150,
-        commutingMileage: 30,
-        businessMileage: 120,
-        notes: '거래처 방문',
-    },
-}
+// const vehicleDetailData = {
+//     vehicleNumber: '123가4567',
+//     vehicleModel: '제네시스',
+//     vehicleData: {
+//         taxStartPeriod: '2024-01-01',
+//         taxEndPeriod: '2024-12-31',
+//         businessId: 1,
+//         businessName: '테스트 주식회사',
+//         businessRegistrationNumber: '123-45-67890',
+//         vehicleNumber: '123가4567',
+//         vehicleTypesName: '제네시스 G80',
+//         usageDate: '2024-01-14',
+//         userId: 1,
+//         departmentName: '영업팀',
+//         name: '홍길동',
+//         mileageBefore: 50000,
+//         mileageAfter: 50150,
+//         totalMileage: 150,
+//         commutingMileage: 30,
+//         businessMileage: 120,
+//         notes: '거래처 방문',
+//     },
+// }
 
 const VehicleDetailPage = () => {
-    return (
-        <div>
-            <VehicleInfo
-                vehicleModel={vehicleDetailData.vehicleModel}
-                vehicleNumber={vehicleDetailData.vehicleNumber}
-            />
-        </div>
-    )
+    return <div></div>
 }
 
 export default VehicleDetailPage
