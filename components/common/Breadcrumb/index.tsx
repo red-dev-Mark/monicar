d137cee29f4dd8a44e56ce92c248bdf909795081
nanafromjeju <nanafromjeju@gmail.com>
2025-01-19 18:00:11 +0900

    feat: aria-current 속성 추가로 접근성 개선 (#72)

--- components/common/Breadcrumb/index.tsx
+++ components/common/Breadcrumb/index.tsx
@@ -17,7 +17,11 @@ const Breadcrumb = ({ type }: BreadcrumbProps) => {
                 {visibleItems.map((item, index) => (
                     <li key={item.label} className={styles.item}>
                         {index > 0 && <span className={styles.divider}>/</span>}
-                        <Link href={item.path} className={styles.link}>
+                        <Link
+                            href={item.path}
+                            className={styles.link}
+                            aria-current={index === visibleItems.length - 1 ? 'page' : undefined}
+                        >
                             {item.label}
                         </Link>
                     </li>
