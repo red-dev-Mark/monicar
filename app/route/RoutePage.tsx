89efe3eaa3755df2216fb8643afbd31491de8c0a
Mark Kwon <redhero8830@gmail.com>
2025-01-20 00:35:25 +0900

    fix: RoutePage document 참조 에러 해결 (#66)
    - 페이지 전체를 dynamic import로 처리하여 클라이언트 사이드 렌더링 적용
    - SSR 방지를 위해 RoutePage 컴포넌트 분리

---
+++ app/route/RoutePage.tsx
@@ -0,0 +1,36 @@
+'use client'
+
+import { useState } from 'react'
+import { Polyline } from 'react-kakao-maps-sdk'
+
+import RouteSearchPanel from '@/app/route/components/RouteSearchPanel'
+import Map from '@/components/domain/map/Map'
+import { useMapControl } from '@/hooks/useMapControl'
+import { styles } from '@/styles/theme.css'
+import { LatLng } from '@/types/location'
+
+import * as vars from './styles.css'
+
+const RoutePage = () => {
+    const [vehiclePaths, setVehiclePaths] = useState<LatLng[]>([])
+
+    const { mapState, updateMapLocation } = useMapControl()
+
+    return (
+        <div className={vars.container}>
+            <Map center={mapState.center} zoom={mapState.level}>
+                <Polyline
+                    path={[vehiclePaths]}
+                    strokeWeight={5}
+                    strokeColor={styles.colors.primary}
+                    strokeOpacity={1}
+                    strokeStyle={'solid'}
+                />
+            </Map>
+
+            <RouteSearchPanel onPathsChange={setVehiclePaths} onMapLocationChange={updateMapLocation} />
+        </div>
+    )
+}
+
+export default RoutePage
