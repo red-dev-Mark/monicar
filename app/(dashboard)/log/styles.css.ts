6608a9744a979e1daf5dd28ddfbd36375ca5a2c8
nanafromjeju <nanafromjeju@gmail.com>
2025-01-20 13:44:20 +0900

    refactor(log): 불필요 스타일 코드 제거 (#79)

--- app/(dashboard)/log/styles.css.ts
+++ app/(dashboard)/log/styles.css.ts
@@ -3,15 +3,12 @@ import { style } from '@vanilla-extract/css'
 export const container = style({
     height: '100vh',
     padding: '20px',
-    display: 'flex',
-    flexDirection: 'column',
+    overflowY: 'auto',
 })
 
 export const contents = style({
     display: 'flex',
     flexDirection: 'column',
     gap: '16px',
-    overflowY: 'auto',
-    height: '100%',
     marginTop: '40px',
 })
