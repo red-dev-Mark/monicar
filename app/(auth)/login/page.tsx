24f4c73b6e123a782d1fed99ea14ece4ece3f9e9
Mark Kwon <redhero8830@gmail.com>
2025-01-21 10:20:17 +0900

    design: 로그인 페이지 UI 구현 (#88)
    - 로그인 폼 레이아웃 구현
    - 입력 필드 스타일링 추가

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -8,12 +8,20 @@ import * as styles from './styles.css'
 const LoginPage = () => {
     return (
         <div className={styles.container}>
-            <div className={styles.swiper}></div>
+            <div className={styles.swiper}>
+                <p className={styles.introduceMessage}>
+                    실시간 지도 기반의 <br />
+                    차량 관제 서비스를 경험해 보세요.
+                </p>
+                <Image src={'/images/sign-in-1.png'} width={800} height={600} alt='박스로고' />
+            </div>
+
             <div className={styles.formContainer}>
                 <div className={styles.logoContainer}>
-                    <Image src={'/logo.png'} width={100} height={50} alt='박스로고' />
-                    <Image src={'/text-logo.png'} width={40} height={30} alt='텍스트로고' />
+                    <Image src={'/logo.png'} width={180} height={120} alt='박스로고' />
+                    <Image src={'/text-logo.png'} width={200} height={40} alt='텍스트로고' />
                 </div>
+
                 <div className={styles.form}>
                     <SignInInput icon='/icons/sign-in-user-icon.svg' placeholder='아이디를 입력해주세요' />
                     <SignInInput
@@ -21,11 +29,15 @@ const LoginPage = () => {
                         type='password'
                         placeholder='비밀번호를 입력해주세요'
                     />
-                    <RoundButton size='large' color='secondary' className={styles.resetButton}>
-                        로그인
-                    </RoundButton>
-                    <p>
-                        비밀번호를 잊으셨나요?<span>관리자에게 문의해주세요.</span>
+
+                    <div className={styles.buttonWrapper}>
+                        <RoundButton size='large' color='secondary' className={styles.resetButton}>
+                            로그인
+                        </RoundButton>
+                    </div>
+
+                    <p className={styles.description}>
+                        비밀번호를 잊으셨나요? <span className={styles.bold}>관리자에게 문의해주세요.</span>
                     </p>
                 </div>
             </div>
--- app/(auth)/login/styles.css.ts
+++ app/(auth)/login/styles.css.ts
@@ -5,31 +5,64 @@ import { vars } from '@/styles/theme.css'
 export const container = style({
     height: '100%',
     display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
     backgroundImage: `url('/images/sign-in-background-desktop.jpg')`,
     backgroundSize: 'cover',
 })
 
-export const swiper = style({})
+export const swiper = style({
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+})
+export const introduceMessage = style({
+    color: vars.colors.white,
+    textAlign: 'center',
+    fontSize: vars.fontSizes.xlarge,
+    fontWeight: vars.fontWeights.bold,
+})
 
 export const formContainer = style({
     display: 'flex',
-    gap: '24px',
+    gap: '36px',
     flexDirection: 'column',
 })
 
-export const logoContainer = style({})
+export const logoContainer = style({
+    display: 'flex',
+    gap: '12px',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+})
 
 export const form = style({
     display: 'flex',
     gap: '18px',
     flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
 })
-// export const container = style({})
-// export const container = style({})
-// export const container = style({})
+export const buttonWrapper = style({
+    width: '100%',
+    marginTop: '12px',
+})
+export const description = style({
+    color: vars.colors.white,
+    fontSize: vars.fontSizes.xsmall,
+})
+
+export const bold = style({
+    fontWeight: vars.fontWeights.bold,
+})
 
 export const resetButton = style({
     border: '0 !important',
     backgroundColor: `${vars.colors.transparent[800]} !important`,
     color: `${vars.colors.primary} !important`,
 })
+
+// 하얀 로고
+// 이미지 자르기
