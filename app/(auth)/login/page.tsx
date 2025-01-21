080d1a13eaf1e31ae80243693f2efdf388868c0a
Mark Kwon <redhero8830@gmail.com>
2025-01-21 09:25:17 +0900

    design: 로그인 페이지 레이아웃 작성 (#88)

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import Image from 'next/image'
+
+import { RoundButton } from '@/components/common/Button/RoundButton'
+import SignInInput from '@/components/common/Input/SignInInput'
+
+import * as styles from './styles.css'
 
 const LoginPage = () => {
-    return <div>LoginPage</div>
+    return (
+        <div className={styles.container}>
+            <div></div>
+            <div>
+                <div>
+                    <Image src={'/logo.png'} width={100} height={50} alt='박스로고' />
+                    <Image src={'/text-logo.png'} width={40} height={30} alt='텍스트로고' />
+                </div>
+                <div>
+                    <SignInInput icon='/icons/sign-in-user-icon.svg' />
+                    <SignInInput icon='/icons/sign-in-lock-icon.svg' type='password' />
+                    <RoundButton size='large' color='secondary'>
+                        로그인
+                    </RoundButton>
+                    <p></p>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default LoginPage
---
+++ app/(auth)/login/styles.css.ts
@@ -0,0 +1,7 @@
+import { style } from '@vanilla-extract/css'
+
+export const container = style({
+    height: '100%',
+    backgroundImage: `url('/images/sign-in-background-desktop.jpg')`,
+    backgroundSize: 'cover',
+})
