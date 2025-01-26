148080bde3be9ec6773202612d5fbd57fc29e063
nanafromjeju <nanafromjeju@gmail.com>
2025-01-26 23:09:52 +0900

    chore: `react-datepicker` 패키지 설치 (#81)

--- app/(main)/log/[id]/page.tsx
+++ app/(main)/log/[id]/page.tsx
@@ -1,5 +1,8 @@
 'use client'
 
+import { useState } from 'react'
+import DatePicker from 'react-datepicker'
+
 import Breadcrumb from '@/components/common/Breadcrumb'
 import LinkButton from '@/components/common/Button/LinkButton'
 import { RoundButton } from '@/components/common/Button/RoundButton'
@@ -42,9 +45,12 @@ const DetailPage = () => {
         ],
     }
 
+    const [startDate, setStartDate] = useState<Date | null>(new Date())
+
     return (
         <div className={styles.container}>
             <Breadcrumb type={'운행일지'} />
+            <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} dateFormat='yyyy.MM.dd' />
             <div className={styles.buttonGroup}>
                 <RoundButton color='secondary' size={'small'}>
                     엑셀
@@ -55,116 +61,125 @@ const DetailPage = () => {
             </div>
 
             <table>
-                <tr>
-                    <th scope='row' className={styles.tableHeader}>
-                        자동차 등록번호
-                    </th>
-                    <th scope='row' className={styles.tableHeader}>
-                        차량종류
-                    </th>
-                </tr>
-                <tr>
-                    <td className={styles.tableCell}>{mockData.vehicleType.vehicleNumber}</td>
-                    <td className={styles.tableCell}>{mockData.vehicleType.vehicleModel}</td>
-                </tr>
-            </table>
-
-            <table>
-                <tr>
-                    <th scope='row' rowSpan={3} className={styles.tableHeader}>
-                        과세기간
-                    </th>
-                    <td rowSpan={3} className={styles.tableCell}>
-                        {mockData.taxStartPeriod} - {mockData.taxEndPeriod}
-                    </td>
-                    <th scope='row' rowSpan={2} className={styles.tableHeader}>
-                        업무승용차 운행기록부
-                    </th>
-                    <th scope='row' className={styles.tableHeader}>
-                        상호명
-                    </th>
-                    <td className={styles.tableCell}>{mockData.businessInfo.businessName}</td>
-                </tr>
-                <tr>
-                    <th scope='row' className={styles.tableHeader}>
-                        사업자 등록번호
-                    </th>
-                    <td className={styles.tableCell}>{mockData.businessInfo.businessRegistrationNumber}</td>
-                </tr>
-            </table>
-
-            <table>
-                <tr>
-                    <th scope='row' rowSpan={2} className={styles.tableHeader}>
-                        사용일자
-                    </th>
-                    <th scope='row' colSpan={8} className={styles.tableHeader}>
-                        운행내역
-                    </th>
-                </tr>
-                <tr>
-                    <th scope='row' className={styles.tableHeader}>
-                        부서
-                    </th>
-                    <th scope='row' className={styles.tableHeader}>
-                        성명
-                    </th>
-                    <th scope='row' className={styles.tableHeader}>
-                        전 계기판 거리
-                    </th>
-                    <th scope='row' className={styles.tableHeader}>
-                        후 계기판 거리
-                    </th>
-                    <th scope='row' className={styles.tableHeader}>
-                        주행거리
-                    </th>
-                    <th scope='row' className={styles.tableHeader}>
-                        출근용
-                    </th>
-                    <th scope='row' className={styles.tableHeader}>
-                        일반업무용
-                    </th>
-                    <th scope='row' className={styles.tableHeader}>
-                        비고
-                    </th>
-                </tr>
-                {mockData.records.map((data) => (
-                    <tr key={data.drivingInfo.drivingAfter}>
-                        <td className={styles.tableCell}>{data.usageDate}</td>
-                        <td className={styles.tableCell}>{data.user.departmentName}</td>
-                        <td className={styles.tableCell}>{data.user.name}</td>
-                        <td className={styles.tableCell}>{data.drivingInfo.drivingBefore}km</td>
-                        <td className={styles.tableCell}>{data.drivingInfo.drivingAfter}km</td>
-                        <td className={styles.tableCell}>{data.drivingInfo.totalDriving}km</td>
-                        <td className={styles.tableCell}>
-                            {data.drivingInfo.businessDrivingDetails.usePurpose === 'COMMUTE'
-                                ? data.drivingInfo.businessDrivingDetails.drivingDistance + 'km'
-                                : ''}
-                        </td>
-                        <td className={styles.tableCell}>
-                            {data.drivingInfo.businessDrivingDetails.usePurpose !== 'COMMUTE'
-                                ? data.drivingInfo.businessDrivingDetails.drivingDistance + 'km'
-                                : ''}
-                        </td>
-                        <td className={styles.tableCell}>{data.drivingInfo.notes}</td>
+                <tbody>
+                    <tr>
+                        <th scope='row' className={styles.tableHeader}>
+                            자동차 등록번호
+                        </th>
+                        <th scope='row' className={styles.tableHeader}>
+                            차량종류
+                        </th>
                     </tr>
-                ))}
+                    <tr>
+                        <td className={styles.tableCell}>{mockData.vehicleType.vehicleNumber}</td>
+                        <td className={styles.tableCell}>{mockData.vehicleType.vehicleModel}</td>
+                    </tr>
+                </tbody>
             </table>
+
             <table>
-                <tr>
-                    <th scope='row' className={styles.tableHeader}>
-                        과세기간 총 주행 거리
-                    </th>
-                    <td className={styles.tableCell}>1,000km</td>
-                    <th scope='row' className={styles.tableHeader}>
-                        과세기간 업무용 사용 거리
-                    </th>
-                    <td className={styles.tableCell}>1,000km</td>
-                    <th scope='row' className={styles.tableHeader}>
-                        업무사용비율
-                    </th>
-                    <td className={styles.tableCell}>100%</td>
-                </tr>
+                <tbody>
+                    <tr>
+                        <th scope='row' rowSpan={3} className={styles.tableHeader}>
+                            과세기간
+                        </th>
+                        <td rowSpan={3} className={styles.tableCell}>
+                            {mockData.taxStartPeriod} - {mockData.taxEndPeriod}
+                        </td>
+                        <th scope='row' rowSpan={2} className={styles.tableHeader}>
+                            업무승용차 운행기록부
+                        </th>
+                        <th scope='row' className={styles.tableHeader}>
+                            상호명
+                        </th>
+                        <td className={styles.tableCell}>{mockData.businessInfo.businessName}</td>
+                    </tr>
+                    <tr>
+                        <th scope='row' className={styles.tableHeader}>
+                            사업자 등록번호
+                        </th>
+                        <td className={styles.tableCell}>{mockData.businessInfo.businessRegistrationNumber}</td>
+                    </tr>
+                </tbody>
+            </table>
+
+            <table>
+                <tbody>
+                    <tr>
+                        <th scope='row' rowSpan={2} className={styles.tableHeader}>
+                            사용일자
+                        </th>
+                        <th scope='row' colSpan={8} className={styles.tableHeader}>
+                            운행내역
+                        </th>
+                    </tr>
+                    <tr>
+                        <th scope='row' className={styles.tableHeader}>
+                            부서
+                        </th>
+                        <th scope='row' className={styles.tableHeader}>
+                            성명
+                        </th>
+                        <th scope='row' className={styles.tableHeader}>
+                            전 계기판 거리
+                        </th>
+                        <th scope='row' className={styles.tableHeader}>
+                            후 계기판 거리
+                        </th>
+                        <th scope='row' className={styles.tableHeader}>
+                            주행거리
+                        </th>
+                        <th scope='row' className={styles.tableHeader}>
+                            출근용
+                        </th>
+                        <th scope='row' className={styles.tableHeader}>
+                            일반업무용
+                        </th>
+                        <th scope='row' className={styles.tableHeader}>
+                            비고
+                        </th>
+                    </tr>
+                    {mockData.records.map((data) => (
+                        <tr key={data.drivingInfo.drivingAfter}>
+                            <td className={styles.tableCell}>{data.usageDate}</td>
+                            <td className={styles.tableCell}>{data.user.departmentName}</td>
+                            <td className={styles.tableCell}>{data.user.name}</td>
+                            <td className={styles.tableCell}>{data.drivingInfo.drivingBefore}km</td>
+                            <td className={styles.tableCell}>{data.drivingInfo.drivingAfter}km</td>
+                            <td className={styles.tableCell}>{data.drivingInfo.totalDriving}km</td>
+                            <td className={styles.tableCell}>
+                                {data.drivingInfo.businessDrivingDetails.usePurpose === 'COMMUTE'
+                                    ? data.drivingInfo.businessDrivingDetails.drivingDistance + 'km'
+                                    : ''}
+                            </td>
+                            <td className={styles.tableCell}>
+                                {data.drivingInfo.businessDrivingDetails.usePurpose !== 'COMMUTE'
+                                    ? data.drivingInfo.businessDrivingDetails.drivingDistance + 'km'
+                                    : ''}
+                            </td>
+                            <td className={styles.tableCell}>{data.drivingInfo.notes}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+
+            <table>
+                <tbody>
+                    <tr>
+                        <th scope='row' className={styles.tableHeader}>
+                            과세기간 총 주행 거리
+                        </th>
+                        <td className={styles.tableCell}>1,000km</td>
+                        <th scope='row' className={styles.tableHeader}>
+                            과세기간 업무용 사용 거리
+                        </th>
+                        <td className={styles.tableCell}>1,000km</td>
+                        <th scope='row' className={styles.tableHeader}>
+                            업무사용비율
+                        </th>
+                        <td className={styles.tableCell}>100%</td>
+                    </tr>
+                </tbody>
             </table>
 
             <LinkButton href={'/log'} className={styles.linkButton}>
