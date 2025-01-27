9b529e15f2e1eed55fbfb75b119625b58a19781f
Mark Kwon <redhero8830@gmail.com>
2025-01-27 21:10:18 +0900

    chore: 수정된 로그인 방식에 따라 Next.js Middleware 활성화 (#92)

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,22 @@
-// import { NextRequest, NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 
-export const middleware = () => {
-    // export async function middleware(request: NextRequest) {
-    // const accessToken = request.cookies.getAll()
-    // const { pathname } = request.nextUrl
-    // const protectedRoutes = ['/dashboard', '/log', '/route', '/location']
-    // if (protectedRoutes.some((route) => pathname.startsWith(route))) {
-    //     if (!accessToken) {
-    //         return NextResponse.redirect(new URL('/signin', request.url))
-    //     }
-    // }
-    // if (pathname === '/signin' && accessToken) {
-    //     return NextResponse.redirect(new URL('/dashboard', request.url))
-    // }
-    // return NextResponse.next()
+export async function middleware(request: NextRequest) {
+    const accessToken = request.cookies.get('access_token')
+    const { pathname } = request.nextUrl
+
+    const protectedRoutes = ['/dashboard', '/log', '/route', '/location']
+
+    if (protectedRoutes.some((route) => pathname.startsWith(route))) {
+        if (!accessToken) {
+            return NextResponse.redirect(new URL('/signin', request.url))
+        }
+    }
+
+    if (pathname === '/signin' && accessToken) {
+        return NextResponse.redirect(new URL('/dashboard', request.url))
+    }
+
+    return NextResponse.next()
 }
 
 export const config = {
