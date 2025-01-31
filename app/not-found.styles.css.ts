b13689c26752419543f98e063570cbd0cfc63bbe
Mark Kwon <redhero8830@gmail.com>
2025-01-31 20:05:34 +0900

    design: PageNotFound 페이지 퍼블리싱 (#113)

--- app/not-found.styles.css.ts
+++ app/not-found.styles.css.ts
@@ -2,29 +2,59 @@ import { style } from '@vanilla-extract/css'
 
 import { vars } from '@/styles/theme.css'
 
-export const container = style({
+export const page = style({
     width: '100%',
-    height: '100%',
-    // display: 'flex',
-    // gap: '244px',
-    // justifyContent: 'center',
-    // alignItems: 'center',
-    // backgroundImage: `url('/images/page-not-found-desktop.jpg')`,
+    height: '100vh',
+    background: `linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.1)), url('/images/page-not-found-desktop.jpg')`,
     backgroundSize: 'cover',
-    backgroundImage: [
-        'linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.5))',
-        `url('/images/page-not-found-desktop.jpg')`,
-    ],
+
     display: 'flex',
     alignItems: 'center',
+
+    selectors: {
+        '&:after': {
+            backgroundColor: '#00000050',
+        },
+    },
+})
+
+export const container = style({
+    marginLeft: '120px',
+    display: 'flex',
+    gap: '380px',
+    flexDirection: 'column',
 })
 
 export const content = style({
-    marginLeft: '80px',
+    display: 'flex',
+    gap: '8px',
+    flexDirection: 'column',
+})
+
+export const pageNotFoundText = style({
+    marginTop: '4px',
+    color: vars.colors.white,
+    fontSize: '48px',
+    fontWeight: vars.fontWeights.bold,
 })
 
 export const title = style({
+    marginLeft: '4px',
     color: vars.colors.white,
-    fontSize: vars.fontSizes.xxlarge,
+    fontSize: vars.fontSizes.xlarge,
     fontWeight: vars.fontWeights.bold,
+})
+
+export const introduce = style({
+    marginLeft: '4px',
+    marginBottom: '22px',
+    color: vars.colors.white,
+    fontWeight: vars.fontWeights.bold,
+    whiteSpace: 'pre-line',
+    letterSpacing: '0.8px',
+})
+
+export const button = style({
+    color: `${vars.colors.primary} !important`,
+    backgroundColor: vars.colors.white,
 })
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,19 +1,28 @@
+import Image from 'next/image'
 import Link from 'next/link'
+
+import { RoundButton } from '@/components/common/Button/RoundButton'
 
 import * as styles from './not-found.styles.css'
 
 const NotFound = () => {
     return (
-        <div className={styles.container}>
-            <div className={styles.content}>
+        <div className={styles.page}>
+            <div className={styles.container}>
                 <div>
-                    <h1 className={styles.title}>Page Not Found</h1>
+                    <Image src={'/white-logo.png'} width={120} height={80} alt='박스로고' />
+                    <h1 className={styles.pageNotFoundText}>Page Not Found</h1>
                 </div>
 
-                <div>
-                    <h3>페이지를 찾을 수 없습니다.</h3>
-                    <p>모니카와 함께 차량을 조회하고 경로를 탐색해보세요.</p>
-                    <Link href='/dashboard'>홈으로 돌아가기</Link>
+                <div className={styles.content}>
+                    <h3 className={styles.title}>페이지를 찾을 수 없습니다.</h3>
+                    <p className={styles.introduce}>{`모니카와 함께 차량을 조회하고 경로를\n탐색해보세요.`}</p>
+
+                    <Link href='/dashboard'>
+                        <RoundButton size='large' color='primary' className={styles.button}>
+                            홈으로 돌아가기
+                        </RoundButton>
+                    </Link>
                 </div>
             </div>
         </div>
