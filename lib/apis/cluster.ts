2ea54e4ba82939023bb2ffb651df8470e91ada86
Mark Kwon <redhero8830@gmail.com>
2025-02-02 06:01:16 +0900

    feat: 클러스터링 응답 데이터 정규화 유틸 함수 구현 (#98)

--- lib/apis/cluster.ts
+++ lib/apis/cluster.ts
@@ -1,11 +1,12 @@
 import { httpClient } from '@/lib/apis/client'
-import { denormalizeCoordinate, getNormalizedZoomLevel } from '@/lib/utils/normalize'
-import { ClusterInfoModel, MapState } from '@/types/map'
+import { denormalizeCoordinate, getNormalizedZoomLevel, normalizeClusterResponse } from '@/lib/utils/normalize'
+import { ClusterPoint, MapState } from '@/types/map'
 
 export const clusterService = {
     // 뷰포트 영역 내 클러스터링 정보 조회
-    getClusterInfo: async ({ level, swCoord, neCoord }: MapState) => {
-        if (!level || !swCoord || !neCoord) return
+
+    getClusterInfo: async ({ level, swCoord, neCoord }: MapState): Promise<ClusterPoint[]> => {
+        if (!level || !swCoord || !neCoord) return []
 
         const params = {
             level: getNormalizedZoomLevel(level),
@@ -19,12 +20,13 @@ export const clusterService = {
             params,
         })
 
-        const filteredClusterInfo = response.data.result?.filter(
-            (cluterInfo: ClusterInfoModel) => cluterInfo.coordinate,
+        const normalizedClusterInfo = normalizeClusterResponse(response.data.result).filter(
+            (cluster) => cluster.count > 10, // TODO: 10 매직넘버 처리
         )
 
-        return filteredClusterInfo
+        return normalizedClusterInfo
     },
+
     // 뷰포트 영역 내 클러스터링 정보(count)가 10 미만일 경우 전체 차량 정보 조회
     getClusterDetailInfo: async ({ level, swCoord, neCoord }: MapState) => {
         if (!level || level > 9 || !swCoord || !neCoord) return
@@ -41,6 +43,8 @@ export const clusterService = {
             params,
         })
 
-        return response.data.result
+        const normalizedClusterInfo = normalizeClusterResponse(response.data.result)
+
+        return normalizedClusterInfo
     },
 }
