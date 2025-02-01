7af374c496f0f2e8dca9ce729ed0b5dbbe5567fa
nanafromjeju <nanafromjeju@gmail.com>
2025-02-02 03:09:18 +0900

    remove(log): ControlBar 컴포넌트 제거 (#108)

--- app/(main)/log/page.tsx
+++ app/(main)/log/page.tsx
@@ -1,12 +1,19 @@
 'use client'
 import { Group, Pagination } from '@mantine/core'
+import Image from 'next/image'
 import { useRouter } from 'next/navigation'
 import { useState } from 'react'
 
 import Breadcrumb from '@/components/common/Breadcrumb'
-import ControlBar from '@/components/domain/vehicle/ControlBar'
+import ExcelButton from '@/components/common/Button/ExcelButton'
+import LinkButton from '@/components/common/Button/LinkButton'
+import ControlLayout from '@/components/common/ControlLayout'
+import SearchInput from '@/components/common/Input/SearchInput'
+import Modal from '@/components/common/Modal'
+import { ModalMessageType } from '@/components/common/Modal/types'
 import ListHeader from '@/components/domain/vehicle/ListHeader'
 import { LOG_TITLES } from '@/constants/listHeader'
+import { useSearchSingleVehicle } from '@/hooks/useSearchSingleVehicle'
 
 import ListItem from './components/ListItem/index'
 import { useLogData } from './hooks/useLogData'
@@ -17,6 +24,8 @@ const LogPage = () => {
     const router = useRouter()
     const [activePage, setActivePage] = useState(1)
     const { logData, isLoading, error } = useLogData(activePage - 1)
+    const { modalMessage, isOpen, searchTerm, handleVehicleSearch, handleSearchChange, closeModal } =
+        useSearchSingleVehicle()
 
     const handleExcelButtonClick = () => {
         if (!logData?.content) return
@@ -37,13 +46,40 @@ const LogPage = () => {
     return (
         <div className={styles.container}>
             <Breadcrumb type={'운행기록'} />
+
             <div className={styles.contents}>
-                <ControlBar onExcelButtonClick={handleExcelButtonClick} />
+                <ControlLayout
+                    control={
+                        <SearchInput
+                            icon='/icons/search-icon.svg'
+                            value={searchTerm}
+                            onChange={handleSearchChange}
+                            onSubmit={handleVehicleSearch}
+                        />
+                    }
+                    primaryButton={<ExcelButton onClick={handleExcelButtonClick} />}
+                    secondaryButton={
+                        <LinkButton href={'/log/register'}>
+                            <div className={styles.linkButton}>
+                                <Image src='/icons/white-add-icon.svg' alt='add' width={18} height={18} />
+                                등록
+                            </div>
+                        </LinkButton>
+                    }
+                />
+                <Modal
+                    isOpen={isOpen}
+                    message={modalMessage as ModalMessageType}
+                    variant={{ variant: 'alert', confirmButton: '확인' }}
+                    onClose={closeModal}
+                />
+
                 <ListHeader headerTitles={LOG_TITLES} />
                 {logData?.content.map((log) => (
                     <ListItem key={log.id} data={log} onClick={() => handleItemClick(log.id)} />
                 ))}
             </div>
+
             <div className={styles.pagination}>
                 <Pagination.Root total={logData?.totalPages || 1} value={activePage} onChange={setActivePage}>
                     <Group gap={5} justify='center'>
--- app/(main)/log/styles.css.ts
+++ app/(main)/log/styles.css.ts
@@ -1,5 +1,7 @@
 import { style } from '@vanilla-extract/css'
 
+import { vars } from '@/styles/theme.css'
+
 export const container = style({
     height: '100vh',
     padding: '40px 44px',
@@ -10,6 +12,24 @@ export const contents = style({
     display: 'flex',
     flexDirection: 'column',
     marginTop: '20px',
+})
+
+export const linkButton = style({
+    backgroundColor: vars.colors.primary,
+    color: vars.colors.white,
+    fontWeight: vars.fontWeights.bold,
+    width: '94px',
+    height: '48px',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: '6px',
+    borderRadius: '30px',
+    border: `1px solid ${vars.colors.gray[200]}`,
+    boxShadow: `0px 4px 4px 0px ${vars.colors.shadow[100]}`,
+    ':hover': {
+        opacity: vars.opacity[80],
+    },
 })
 
 export const pagination = style({
