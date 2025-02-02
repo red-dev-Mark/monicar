48e2e912f6f8c58c159f6d18637193ac244a76f7
Mark Kwon <redhero8830@gmail.com>
2025-02-02 22:57:18 +0900

    feat: 환경에 따라 미들웨어 쿠키 체크 로직 분리 (#134)

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,18 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 export const middleware = async (request: NextRequest) => {
-    const accessToken = request.cookies.get('access_token')?.value
-    console.log('----------------------------------------------')
-    console.log('액세스 토큰: ', accessToken)
-    console.log('----------------------------------------------')
-
+    const isDevelopment = process.env.NODE_ENV === 'development'
+    const accessToken = request.cookies.get('access_token')
     const { pathname } = request.nextUrl
+    // console.log('----------------------------------------------')
+    // console.log('액세스 토큰: ', accessToken)
+    // console.log('----------------------------------------------')
 
     const protectedRoutes = ['/dashboard', '/log', '/route', '/location']
+
+    if (isDevelopment) {
+        return NextResponse.next()
+    }
 
     if (protectedRoutes.some((route) => pathname.startsWith(route))) {
         if (!accessToken) {
