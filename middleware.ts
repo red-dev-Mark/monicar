f8283e12e28b4c717116844b53dea45e047dbe7c
Mark Kwon <redhero8830@gmail.com>
2025-02-05 18:02:38 +0900

    fix: 미들웨어 인증 로직 개선 - 리프레시 토큰 체크 추가 (#144)
    - 기존: 액세스 토큰 체크만 진행
    - 변경: 액세스 + 리프레시 토큰 둘 다 체크

--- middleware.ts
+++ middleware.ts
@@ -2,17 +2,19 @@ import { NextRequest, NextResponse } from 'next/server'
 
 export const middleware = async (request: NextRequest) => {
     const accessToken = request.cookies.get('access_token')
+    const refreshToken = request.cookies.get('refresh_token')
 
     const { pathname } = request.nextUrl
 
     console.log('----------------------------------------------')
     console.log('액세스 토큰: ', accessToken)
+    console.log('리프레시 토큰: ', refreshToken)
     console.log('----------------------------------------------')
 
     const protectedRoutes = ['/dashboard', '/log', '/route', '/location']
 
     if (protectedRoutes.some((route) => pathname.startsWith(route))) {
-        if (!accessToken) {
+        if (!accessToken && !refreshToken) {
             return NextResponse.redirect(new URL('/signin', request.url))
         }
     }
