c357e694fa1da28b4f61699b8dddea06df2f5245
nanafromjeju <nanafromjeju@gmail.com>
2025-02-06 15:11:07 +0900

    feat(log): DatePickerInput 조회 날짜 기본값 수정 (#157)

--- app/(main)/log/[id]/hooks/useSearchDate.ts
+++ app/(main)/log/[id]/hooks/useSearchDate.ts
@@ -3,7 +3,10 @@ import { useState } from 'react'
 import { formatToKSTDate } from '@/lib/utils/date'
 
 export const useSearchDate = () => {
-    const [dateRange, setDateRange] = useState<[Date | null, Date | null]>([new Date(), new Date()])
+    const [dateRange, setDateRange] = useState<[Date | null, Date | null]>([
+        new Date(new Date().setFullYear(new Date().getFullYear() - 1)),
+        new Date(),
+    ])
 
     const handleDateRangeChange = (value: [Date | null, Date | null]) => {
         setDateRange(value)
