065fde0b543393cbb25664a86311539ff9549efd
nanafromjeju <nanafromjeju@gmail.com>
2025-02-12 22:21:05 +0900

    fix: Skeleton UI 하이드레이션 에러 해결 (#118)

--- app/(main)/log/components/LogListItemSkeleton/index.tsx
+++ app/(main)/log/components/LogListItemSkeleton/index.tsx
@@ -10,17 +10,17 @@ const LogListItemSkeleton = () => {
                     <Skeleton height={40} mt={4} width='70%' radius='md' />
                 </li>
                 <li className={styles.itemWrapper}>
-                    <Skeleton height={40} mt={4} width={`${Math.floor(60 + Math.random() * 21)}%`} radius='md' />
+                    <Skeleton height={40} mt={4} width='50%' radius='md' />
                 </li>
                 <li className={styles.itemWrapper}>
-                    <Skeleton height={40} mt={4} width={`${Math.floor(60 + Math.random() * 21)}%`} radius='md' />
+                    <Skeleton height={40} mt={4} width='50%' radius='md' />
                 </li>
 
                 <li className={styles.itemWrapper}>
-                    <Skeleton height={40} mt={4} width={`${Math.floor(60 + Math.random() * 21)}%`} radius='md' />
+                    <Skeleton height={40} mt={4} width='50%' radius='md' />
                 </li>
                 <li className={styles.badge}>
-                    <Skeleton height={40} mt={4} width='80%' radius='md' />
+                    <Skeleton height={40} mt={4} width='70%' radius='md' />
                 </li>
             </ul>
             <div className={styles.icon}></div>
