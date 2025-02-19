cfa4ff1857d5c6d967acd5b488ea48fbfe82f65e
nanafromjeju <nanafromjeju@gmail.com>
2025-02-19 20:23:16 +0900

    feat(dashboard): 차량 점검 상태 변경 후 새로고침 적용 (#224)

--- app/(main)/dashboard/inspection/Inspection.tsx
+++ app/(main)/dashboard/inspection/Inspection.tsx
@@ -32,6 +32,7 @@ const Inspection = () => {
     const patchInspectionStatusData = async (alarmId: number) => {
         try {
             await vehicleService.patchInspectionStatus(alarmId)
+            window.location.reload()
         } catch (error) {
             console.error('이건 patch 에러', error)
         }
