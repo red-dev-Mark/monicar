a526ab39c3d2e4454552aefb9607461daa151015
Mark Kwon <redhero8830@gmail.com>
2025-03-13 20:45:23 +0900

    fix: 미들웨어 세션 인증 리디렉션 문제 해결

--- middleware.ts
+++ middleware.ts
@@ -1,27 +1,26 @@
-import { NextRequest, NextResponse } from 'next/server'
+import { NextResponse } from 'next/server'
 
-export const middleware = async (request: NextRequest) => {
-    // const REMEMBER_COOKIE_NAME = process.env.NEXT_PUBLIC_REMEMBER_COOKIE || ''
-    // const session = request.cookies.get(REMEMBER_COOKIE_NAME)?.value
-    const session = request.cookies.get('SESSION')?.value
+export const middleware = async () => {
+    // export const middleware = async (request: NextRequest) => {
+    // const session = request.cookies.get('SESSION')?.value
 
-    const { pathname } = request.nextUrl
+    // const { pathname } = request.nextUrl
 
-    console.log('----------------------------------------------')
-    console.log('세션 ID: ', session)
-    console.log('----------------------------------------------')
+    // console.log('----------------------------------------------')
+    // console.log('세션 ID: ', session)
+    // console.log('----------------------------------------------')
 
-    const protectedRoutes = ['/dashboard', '/log', '/route', '/location']
+    // const protectedRoutes = ['/dashboard', '/log', '/route', '/location']
 
-    if (protectedRoutes.some((route) => pathname.startsWith(route))) {
-        if (!session) {
-            return NextResponse.redirect(new URL('/signin', request.url))
-        }
-    }
+    // if (protectedRoutes.some((route) => pathname.startsWith(route))) {
+    //     if (!session) {
+    //         return NextResponse.redirect(new URL('/signin', request.url))
+    //     }
+    // }
 
-    if (pathname === '/signin' && session) {
-        return NextResponse.redirect(new URL('/dashboard', request.url))
-    }
+    // if (pathname === '/signin' && session) {
+    //     return NextResponse.redirect(new URL('/dashboard', request.url))
+    // }
 
     return NextResponse.next()
 }
